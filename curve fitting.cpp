35d9f1d1af1672c19c4f5978ae699600cb022c28
Habibur Rahman Aasif <156560375+Habibur-02@users.noreply.github.com>
2025-01-06 21:32:02 +0600

    Add files via upload

---
+++ curve fitting.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m;
+    cout<<"How many points"<<'\n';
+
+    cin>>m;
+
+    vector<int>x,y;
+
+    cout<<"Give the value of x and y"<<'\n';
+
+    for(int i=0;i<m;i++)
+    {
+
+
+    }
+
+
+
+
+
+
+
+
+}
